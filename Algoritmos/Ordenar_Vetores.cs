3af752abd14323c2df87d46b590a66c8cf45d37c
Igor Gassen <104468900+Kryotsz@users.noreply.github.com>
2023-04-17 16:49:22 -0300

    Add files via upload

---
+++ Algoritmos/Ordenar_Vetores.cs
@@ -0,0 +1,19 @@
+﻿int[] vetor = new int[10];
+
+for(int i=0;i<10;i++){
+    vetor[i]=int.Parse(Console.ReadLine());
+}
+
+for(int i=0;i<9;i++){
+    for(int j=i+1;j<10;j++){
+        if(vetor[i]>vetor[j]){
+            int aux = vetor[i];
+            vetor[i]=vetor[j];
+            vetor[j]=aux;
+        }
+    }
+}
+
+for(int i=0;i<10;i++){
+    Console.Write(vetor[i]+" ");
+}
---
+++ Algoritmos/Torneio_de_Futebol.cs
@@ -0,0 +1,29 @@
+﻿int maior=0;
+
+Console.WriteLine("------------------");
+Console.WriteLine("CAMPEONATO FUTEBOL");
+Console.WriteLine("------------------");
+
+string[] vetor = new string[3];
+
+for(int i=0;i<3;i++){
+    Console.Write("Nome do "+(i+1)+"° time: ");
+    vetor[i]=Console.ReadLine();
+
+    int tamanho = vetor[i].Length;
+    if(tamanho>maior){
+        maior=tamanho;
+    }
+}
+
+Console.WriteLine("------------------");
+Console.WriteLine("TABELA DE PARTIDAS");
+Console.WriteLine("------------------");
+
+for(int i=0;i<3;i++){
+    for(int j=0;j<3;j++){
+        if(vetor[i]!=vetor[j]){
+            Console.WriteLine(vetor[i].PadRight(maior)+" [ ] x [ ] "+vetor[j]);
+        }
+    }
+}
