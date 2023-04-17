ac95599dc0ac144a651e8ad6107a45a993f199c0
Igor Gassen <104468900+Kryotsz@users.noreply.github.com>
2023-04-17 16:56:14 -0300

    Update Torneio_de_Futebol.cs

--- Algoritmos/Torneio_de_Futebol.cs
+++ Algoritmos/Torneio_de_Futebol.cs
@@ -1,16 +1,20 @@
-﻿int maior=0;
+// variavel de controle de tamanho que posteriormente será usada na formatação do Console.WriteLine
+int maior=0;
 
 Console.WriteLine("------------------");
 Console.WriteLine("CAMPEONATO FUTEBOL");
 Console.WriteLine("------------------");
 
-string[] vetor = new string[3];
-
+// array pra armazenas nome dos times
+string[] times = new string[3];
+// loop pra ler o nome dos times e adicionar no array
 for(int i=0;i<3;i++){
     Console.Write("Nome do "+(i+1)+"° time: ");
-    vetor[i]=Console.ReadLine();
-
-    int tamanho = vetor[i].Length;
+    times[i]=Console.ReadLine();
+    
+    // variavel pra armazenar o numero de caracteres do nome do time
+    int tamanho = times[i].Length;
+    // comparação pra estabelecer qual nome é maior
     if(tamanho>maior){
         maior=tamanho;
     }
@@ -20,10 +24,11 @@ Console.WriteLine("------------------");
 Console.WriteLine("TABELA DE PARTIDAS");
 Console.WriteLine("------------------");
 
+// loop pra escrever na tela todas as combinações de jogos possíveis
 for(int i=0;i<3;i++){
     for(int j=0;j<3;j++){
-        if(vetor[i]!=vetor[j]){
-            Console.WriteLine(vetor[i].PadRight(maior)+" [ ] x [ ] "+vetor[j]);
+        if(times[i]!=times[j]){
+            Console.WriteLine(times[i].PadRight(maior)+" [ ] x [ ] "+times[j]);
         }
     }
 }
