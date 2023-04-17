de55c52ece316aeece78b55d9fbe42f4223fb7dd
Igor Gassen <104468900+Kryotsz@users.noreply.github.com>
2023-04-17 16:58:17 -0300

    Update Ordenar_Vetores.cs

--- Algoritmos/Ordenar_Vetores.cs
+++ Algoritmos/Ordenar_Vetores.cs
@@ -1,9 +1,11 @@
-﻿int[] vetor = new int[10];
-
+// cria um array pra armazenar números inteiros em qualquer ordem
+int[] vetor = new int[10];
+// preenche o array com os números digitados pelo usuário
 for(int i=0;i<10;i++){
     vetor[i]=int.Parse(Console.ReadLine());
 }
 
+// organiza o array de forma crescente
 for(int i=0;i<9;i++){
     for(int j=i+1;j<10;j++){
         if(vetor[i]>vetor[j]){
@@ -14,6 +16,7 @@ for(int i=0;i<9;i++){
     }
 }
 
+// escreve na tela o array que agora está em ordem crescente
 for(int i=0;i<10;i++){
     Console.Write(vetor[i]+" ");
 }
